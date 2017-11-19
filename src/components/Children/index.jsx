c9fb4e7c65e3ede11de7cc51c00698cde661097f
ahnhyunmo <gusahdks@gmail.com>
2017-11-19 12:47:07 +0900

    img사용 예제 추가

--- src/components/Children/index.jsx
+++ src/components/Children/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import workImage from 'assets/image/work-image.jpg';
+import './Children.scss';
 
 function Children() {
 	return(
 		<div>
 			Children
+			<img src={workImage} alt="work-image" />
 		</div>
 	);
 }
