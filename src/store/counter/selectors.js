af6f30901d6bc5f520ec7c1d87b042df5a3fc8b0
ahnhyunmo <gusahdks@gmail.com>
2017-11-19 16:17:45 +0900

    immutable도입 및 redux-immutable을 이용해 store내부를 immutable객체로 변경

--- src/store/counter/selectors.js
+++ src/store/counter/selectors.js
@@ -1,8 +1,8 @@
 import { createSelector } from 'reselect';
 
-export const counterSelector = state => state.counter;
+export const counterSelector = state => state.get('counter');
 
 export const valueSelector = createSelector(
 	counterSelector,
-	counter => counter.value,
+	counter => counter.get('value'),
 );
