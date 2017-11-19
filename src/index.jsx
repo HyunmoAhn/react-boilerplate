756c9a030cac7f2bb829a6437f111e37c6b468c3
ahnhyunmo <gusahdks@gmail.com>
2017-11-19 15:56:01 +0900

    Redux 설정 및 예제 코드인 counter를 redux 활용하도록 변경
    - store 생성
    - reducer 생성
    - actionTypes 명시
    - action 생성
    - reducer값을 selector를 통해 가져오기

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes';
+import store from './store';
 import './scss/index.scss';
 
 ReactDOM.render(
-	<Router history={browserHistory} routes={routes()} />,
-	document.getElementById('root')
+	<Provider store={store}>
+		<div>
+			<Router history={browserHistory} routes={routes()} />,
+		</div>
+	</Provider>,
+	document.getElementById('root'),
 );
