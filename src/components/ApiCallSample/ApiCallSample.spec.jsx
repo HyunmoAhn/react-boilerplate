11b433ed414a87a51330a9cd6d893262f9b990eb
ahnhyunmo <gusahdks@gmail.com>
2017-12-05 23:53:01 +0900

    테스트 코드에 대한 샘플 추가

---
+++ src/components/ApiCallSample/ApiCallSample.spec.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ApiCallSample from './';
+
+describe('<ApiCallSample />', () => {
+	it('should match to snapshot when render default', () => {
+		const wrapper = shallow(<ApiCallSample />);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+});
